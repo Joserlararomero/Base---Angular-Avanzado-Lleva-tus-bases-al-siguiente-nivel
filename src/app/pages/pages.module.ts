ab92f204ea75e898dd57a729b4997d2700d5669e
Jose Lara <joserlararomero@gmail.com>
2019-05-16 17:08:37 -0400

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { PagesComponent } from './pages.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+//Rutas Hijas
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent
+    ],
+    exports: [
+        DashboardComponent
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ],
+})
+
+export class PagesModule { }
